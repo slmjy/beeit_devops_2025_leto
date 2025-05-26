2aa6a774c352aa9f682db5fc8d2584be08a5f3e8
Adam Pastrňák <adam.pastrnak@email.cz>
2025-05-26 17:32:04 +0200

    Přidán skript linux_cli.sh který vypíše informace o OS

---
+++ linux_cli.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Vypíše název aktuálního shellu
+echo "Používaný shell: $SHELL"
+
+# Vypíše jméno aktuálního uživatele
+echo "Aktuální uživatel: $USER"
+
+# Vypíše verzi jádra Linuxu
+echo "OS:"
+cat /etc/os-release
