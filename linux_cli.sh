d10e665104a954dd4fcb527d4c9a1ebdf23f3bff
Zuzike <darigjorkinen@gmail.com>
2025-05-28 17:29:23 +0200

    Z2.1. Linuw prikazy, update souboru 1.cast

--- linux_cli.sh
+++ linux_cli.sh
@@ -1,3 +1,10 @@
 echo $SHELL
 whoami
-cat etc/os-release
+cat /etc/os-release
+cd /usr
+mkdir -p adresar/podadresar/posledniadresar 
+cd adresar/podadresar/posledniadresar
+echo "Ahoj ze sveta Linux" > soubor.txt
+ln -s soubor.txt /tmp/softLink
+cp soubor.txt /tmp
+id
