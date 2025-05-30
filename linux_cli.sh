1d0df63c98ae5071f65f466f70caa8a500f353e7
DopamineLady <nicole.zalisova@gmail.com>
2025-05-30 13:14:49 +0200

    Linux task4

--- linux_cli.sh
+++ linux_cli.sh
@@ -1,5 +1,15 @@
-echo "Shell: $SHELL" -> Shell: /bin/zsh
-whoami -> nicolezalisova
-sw_vers(macOS) -> ProductName:		macOS
-ProductVersion:		15.3.1
-BuildVersion:		24D70
+echo "Shell: $SHELL"
+whoami
+sw_vers
+# musela jsem pouzit ~ pro vytvoreni v domacim adresari, jinak jsem ani se sudo nemela prava..
+mkdir -p ~/usr/adresar/podadresar/posledniadresar/
+cd ~/usr/adresar/podadresar/posledniadresar/
+touch soubor.txt
+echo "Ahoj ze sveta Linux" > soubor.txt
+ln -s soubor.txt /tmp/sofLink
+cp soubor.txt /tmp/
+id -u
+id -g
+chmod o=r
+# tohle jsem teda popravde musela najit
+chmod -h u+x,go= /tmp/sofLink
