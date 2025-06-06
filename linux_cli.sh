307024105e368731b4fb2903f9493d68a9de4a46
DopamineLady <nicole.zalisova@gmail.com>
2025-06-06 20:37:44 +0200

    Linux task5

--- linux_cli.sh
+++ linux_cli.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 echo "Shell: $SHELL"
 whoami
 sw_vers
@@ -13,3 +14,104 @@ id -g
 chmod o=r
 # tohle jsem teda popravde musela najit
 chmod -h u+x,go= /tmp/sofLink
+
+create_directory() {
+    if mkdir -p "$1"; then
+        echo "Vytvoren adresar $1"
+    else 
+        echo "Doslo k chybe"
+    fi
+}
+
+create_link() {
+    TYPE=$1
+    SOURCE=$2
+    TARGET=$3
+#-e existuje
+     if [ -e "$TARGET" ]; then
+        echo "Link '$TARGET' uz existuje"
+        exit 2
+    fi
+
+    if [ "$TYPE" = "soft" ]; then
+        ln -s "$SOURCE" "$TARGET"
+        echo "Vytvoren soft link"
+    else
+        ln "$SOURCE" "$TARGET"
+        echo "Vytvoren hard link"
+    fi
+}
+
+info_user() {
+    echo "User ID: $(id -u)"
+    echo "Hostname: $(hostname)"
+    echo "Date: $(date)"
+}
+
+list_update() {
+    echo "Vsechny balicky, ktere maji update:"
+    apt list --upgradable
+}
+
+update_upgrade() {
+    echo "Update/Upgrade balicku"
+    sudo apt update && sudo apt upgrade
+}
+
+
+LOG_TO_FILE=false
+LOG_FILE="./log_file.txt"
+
+log() {
+    if $LOG_TO_FILE; then
+        echo "$1" >> "$LOG_FILE"
+    else
+        echo "$1"
+    fi
+}
+
+print_help() {
+    cat << EOF
+
+Zkratky:
+  -m PATH           Vytvoreni adresare, popripade jeho zanorene struktury napr. např. ./script.sh -m /usr/novyadresar/podadresar
+  -l TYPE SOURCE TARGET   Vytvori link (soft nebo hard) 
+  -i                 Zakladni info o uzivateli
+  -u                 Vylistuje vsechny balicky, ktere maji update
+  -g                 Update a upgrade balicku
+  -s                 Zapne logovani do souboru nebo STDOUT
+  -h                 Zobrazi napovedu
+EOF
+}
+
+while getopts "m:l:iugo:h" opt; do
+    case {$opt} in
+        m)
+            create_directory "$OPTARG";;
+            exit 0
+        l)
+            create_link "$OPTARG";;
+            exit 0
+        i)
+            info_user;;
+            exit 0
+        u)
+            list_update;;
+            exit 0
+        g)
+            update_upgrade;;
+            exit 0
+        o)
+            LOG_TO_FILE=true
+            LOG_FILE="$OPTARG";;
+            ecit 0
+        h)
+            print_help;;
+            exit 0
+        *)
+            echo "Neco se pokazilo. Dej -h pro napovedu";;
+            exit 1
+            
+    esac
+done
+
